3610d8aa58333fa4df8a4f42d41af8c8ec9e24a4
koolas <koolas@6beb23dc-64f8-0310-9b18-f4161846aa7e>
2006-02-01 14:37:30 +0000

    +splash added;

--- trunk/KooLiXP/KooLiXP.cpp
+++ trunk/KooLiXP/KooLiXP.cpp
@@ -3,20 +3,24 @@
 #include "Useless/UselessLib.h"
 #include "Useless/System/Application.h"
 #include "Useless/GUI/Display/ScreenShooter.h"
+#include "Useless/Graphic/Device/winGDI/GDIScreen.h" //!< used for KooLiXP splash
+#include "Useless/Graphic/Device/winGDI/GDISurface.h" //!< used for KooLiXP splash
 
 #include "koolib/xml/XMLChunksGUI.h"
 #include "koolib/xml/XMLModuleBuiltIn.h"
 #include "koolib/xml/XMLModuleUseless.h"
 #include "koolib/xml/XMLModuleDynamo.h"
 
+#include "resource.h"
+
 #include <direct.h>
 #include <iostream>
 
 namespace Useless {
-    const char * Application::_icon_name = 0;
+    const char * Application::_icon_name = "icon1.ico";
     const char * Application::_app_title = "KooLiXP";
-    const int    Application::_icon_resource_id = 0;
-    const int    Application::_big_icon_resource_id = 0;
+    const int    Application::_icon_resource_id = IDI_ICON1;
+    const int    Application::_big_icon_resource_id = IDI_ICON1;
 
     extern bool G_EnableDDrawScr;
     extern bool G_DisableAltTab;
@@ -40,133 +44,140 @@ std::string get_argv( int i )
     return name;
 }
 
+
 struct KooLiXP
 { 
     Useless::Error _error;
     Useless::ScreenResource::Type _screen;
     bool        _snapshotOnly;
     bool        _helpOnly;
+    bool        _noSplash;
     std::string _file;
     std::string _guiScreen;
-    
+
     KooLiXP()
         : _snapshotOnly( false )
         , _helpOnly( false )
-    {
-        XMLProgram::GlobalsAddRef();
-        Application::SetIdleDT(5);
-        
-        for ( int i=1, I=get_argc(); i < I; ++i )
+        , _noSplash( false )
         {
-            if ( 0 == get_argv(i).find("--"))
+            XMLProgram::GlobalsAddRef();
+            Application::SetIdleDT(5);
+
+            for ( int i=1, I=get_argc(); i < I; ++i )
             {
-                if ( get_argv(i) == "--chroot" )
+                if ( 0 == get_argv(i).find("--"))
                 {
-                    _chdir( get_argv(++i).c_str() );
-                }
-                else if( get_argv(i) == "--snapshot" )
-                {
-                    _snapshotOnly = true;
-                }
-                else if ( get_argv(i) == "--gui-screen" )
-                {
-                    _guiScreen = get_argv(++i);
-                }
-                else if ( std::string::npos != get_argv(i).rfind( "-direct-draw-screen" ))
-                {
-                    Useless::G_EnableDDrawScr = (std::string::npos == get_argv(i).find("--no"));
-                }
-                else if ( std::string::npos != get_argv(i).rfind( "-alt-tab" ))
-                {
-                    Useless::G_DisableAltTab =  ( 0 == get_argv(i).find("--no"));
-                }
-                else if ( std::string::npos != get_argv(i).rfind( "-print-screen" ))
-                {
-                    Useless::G_EnablePrintScr = (std::string::npos == get_argv(i).find("--no"));
-                }
-                else if ( std::string::npos != get_argv(i).rfind( "-debug-keys" ))
-                {
-                    Useless::G_EnableDebugF11 = (std::string::npos == get_argv(i).find("--no"));
-                }
-                else if ( std::string::npos != get_argv(i).rfind( "-viewport-fix" ))
-                {
-                    Useless::G_nVidiaViewport = ( 0 == get_argv(i).find("--no"));
-                }
-                else if ( get_argv(i) == "--idle-time-delta" )
-                {
-                    Useless::Application::SetIdleDT( Useless::to_integer( get_argv(++i)));
-                }
-                else if ( get_argv(i) == "--texel-offsets" )
-                {
-                    G_GLTesselatorStartingTexelOffset = Useless::to_real( get_argv(++i));
-                    G_GLTesselatorEndingTexelOffset = Useless::to_real( get_argv(++i));
-                }
-                else if ( get_argv(i) == "--help" )
-                {
-                    std::stringstream ss;
-                    PutInfo( ss );
-                    PutUsage( ss );
-                    _helpOnly = true;
-                    _error = Useless::Error("%s", ss.str().c_str());
-                }
-                else if ( get_argv(i) == "--" )
-                {
-                    XMLProgram::ExecutionState &gS = XMLProgram::GetGlobals();
-                    XMLProgram::IBlock *pBlock = 0;
-                    gS._currentBlock->AddChunk( L"argv", pBlock = XMLProgram::CreateBlock() );
-                    for ( ++i; i < I; ++i )
+                    if ( get_argv(i) == "--chroot" )
                     {
-                        if ( get_argv(i) == "--" )
+                        _chdir( get_argv(++i).c_str() );
+                    }
+                    else if( get_argv(i) == "--snapshot" )
+                    {
+                        _snapshotOnly = true;
+                    }
+                    else if( get_argv(i) == "--no-splash" )
+                    {
+                        _noSplash = true;
+                    }
+                    else if ( get_argv(i) == "--gui-screen" )
+                    {
+                        _guiScreen = get_argv(++i);
+                    }
+                    else if ( std::string::npos != get_argv(i).rfind( "-direct-draw-screen" ))
+                    {
+                        Useless::G_EnableDDrawScr = (std::string::npos == get_argv(i).find("--no"));
+                    }
+                    else if ( std::string::npos != get_argv(i).rfind( "-alt-tab" ))
+                    {
+                        Useless::G_DisableAltTab =  ( 0 == get_argv(i).find("--no"));
+                    }
+                    else if ( std::string::npos != get_argv(i).rfind( "-print-screen" ))
+                    {
+                        Useless::G_EnablePrintScr = (std::string::npos == get_argv(i).find("--no"));
+                    }
+                    else if ( std::string::npos != get_argv(i).rfind( "-debug-keys" ))
+                    {
+                        Useless::G_EnableDebugF11 = (std::string::npos == get_argv(i).find("--no"));
+                    }
+                    else if ( std::string::npos != get_argv(i).rfind( "-viewport-fix" ))
+                    {
+                        Useless::G_nVidiaViewport = ( 0 == get_argv(i).find("--no"));
+                    }
+                    else if ( get_argv(i) == "--idle-time-delta" )
+                    {
+                        Useless::Application::SetIdleDT( Useless::to_integer( get_argv(++i)));
+                    }
+                    else if ( get_argv(i) == "--texel-offsets" )
+                    {
+                        G_GLTesselatorStartingTexelOffset = Useless::to_real( get_argv(++i));
+                        G_GLTesselatorEndingTexelOffset = Useless::to_real( get_argv(++i));
+                    }
+                    else if ( get_argv(i) == "--help" )
+                    {
+                        std::stringstream ss;
+                        PutInfo( ss );
+                        PutUsage( ss );
+                        _helpOnly = true;
+                        _error = Useless::Error("%s", ss.str().c_str());
+                    }
+                    else if ( get_argv(i) == "--" )
+                    {
+                        XMLProgram::ExecutionState &gS = XMLProgram::GetGlobals();
+                        XMLProgram::IBlock *pBlock = 0;
+                        gS._currentBlock->AddChunk( L"argv", pBlock = XMLProgram::CreateBlock() );
+                        for ( ++i; i < I; ++i )
                         {
-                            break;
+                            if ( get_argv(i) == "--" )
+                            {
+                                break;
+                            }
+                            int p = get_argv(i).find("=");
+                            if ( std::string::npos != p )
+                            {
+                                std::string name  = get_argv(i).substr(0,p);
+                                std::string value = get_argv(i).substr(p+1,-1);
+                                pBlock->AddChunk( TextUtf8( name ), XMLProgram::CreateValue( TextUtf8( value )));
+                            }
+                            else
+                            {
+                                pBlock->AddChunk( get_argv(i), XMLProgram::CreateValue( 1 ));
+                            }
                         }
-                        int p = get_argv(i).find("=");
-                        if ( std::string::npos != p )
-                        {
-                            std::string name  = get_argv(i).substr(0,p);
-                            std::string value = get_argv(i).substr(p+1,-1);
-                            pBlock->AddChunk( TextUtf8( name ), XMLProgram::CreateValue( TextUtf8( value )));
-                        }
-                        else
-                        {
-                            pBlock->AddChunk( get_argv(i), XMLProgram::CreateValue( 1 ));
-                        }
+                    }
+                    else
+                    {
+                        std::stringstream ss;
+                        PutInfo( ss );
+                        PutError( ss, "Bad option '"+ get_argv(i) +"' given.\n\nFor usage type KooLiXP --help.");
+                        _helpOnly = true;
+                        _error = Useless::Error("%s", ss.str().c_str());
                     }
                 }
                 else
                 {
+                    _file = get_argv(i);
+                }
+            }
+            if ( !_helpOnly )
+            {
+                if ( _file.empty() )
+                {
                     std::stringstream ss;
                     PutInfo( ss );
-                    PutError( ss, "Bad option '"+ get_argv(i) +"' given.\n\nFor usage type KooLiXP --help.");
+                    PutError( ss, "No filename given.\n\nFor usage type KooLiXP --help.");
+                    _helpOnly = true;
+                    _error = Useless::Error("%s", ss.str().c_str());
+                }
+                else if ( !Useless::IFS::Instance().Exists( _file ))
+                {
+                    std::stringstream ss;
+                    PutInfo( ss );
+                    PutError( ss, "File '"+ _file +"' was not found.\n\nFor usage type KooLiXP --help.");
                     _helpOnly = true;
                     _error = Useless::Error("%s", ss.str().c_str());
                 }
             }
-            else
-            {
-                _file = get_argv(i);
-            }
         }
-        if ( !_helpOnly )
-        {
-            if ( _file.empty() )
-            {
-                std::stringstream ss;
-                PutInfo( ss );
-                PutError( ss, "No filename given.\n\nFor usage type KooLiXP --help.");
-                _helpOnly = true;
-                _error = Useless::Error("%s", ss.str().c_str());
-            }
-            else if ( !Useless::IFS::Instance().Exists( _file ))
-            {
-                std::stringstream ss;
-                PutInfo( ss );
-                PutError( ss, "File '"+ _file +"' was not found.\n\nFor usage type KooLiXP --help.");
-                _helpOnly = true;
-                _error = Useless::Error("%s", ss.str().c_str());
-            }
-        }
-    }
 
     int MakeScreenshot()
     {
@@ -187,7 +198,28 @@ struct KooLiXP
         else
         {
             try {
+                if ( !_noSplash )
+                {
+                    Surf::Properties splashProps;
+                    splashProps.width = 320;
+                    splashProps.height = 240;
+                    splashProps.pixelformat = ImageFormat::B8G8R8A8;
+                    _screen = new GDIScreen( Error("KooLiXP: Starting %s, Please wait...", _file.c_str()).GetError() );
+                    _screen->Reposition( 480, 360 );
+                    _screen->HideDecorations();
+                    _screen->OpenWindowed( splashProps.width, splashProps.height);
+                    _screen->SetClipper( Rect(splashProps.width, splashProps.height) );
+                    GDISurface splashSurf( splashProps, _screen->GetHandle(), IDB_BITMAP1 );
+                    _screen->GetSurface()->Blit( 0, 0, splashSurf, Rect(splashProps.width, splashProps.height) );
+                    _screen->Swap();
+                }
+
                 Useless::CreateFromXMLFile( _file );
+                
+                if ( !!_screen )
+                {
+                    _screen->Close();
+                }
                 _screen = *Useless::ScreenResource("system", "screen");
                 if ( !_guiScreen.empty() )
                 {
@@ -202,13 +234,23 @@ struct KooLiXP
                     return Useless::Application::Run();
                 }
             }
-            catch ( const std::exception &e ) {
+            catch ( const std::exception &e )
+            {
+                if ( !!_screen )
+                {
+                    _screen->Close();
+                }
                 std::stringstream ss;
                 PutInfo( ss );
                 PutError( ss, e.what() );
                 _helpOnly = true;
-                _error = Useless::Error("%s", ss.str().c_str());
-				throw _error;
+                if ( IDYES == MessageBoxA( NULL
+                            , (ss.str() + "\n\nLog this message ?").c_str()
+                            , (_file + " - Fatal Exception").c_str()
+                            , MB_YESNO | MB_ICONEXCLAMATION | MB_SYSTEMMODAL ))
+                {
+                    throw Useless::Error( ss.str().c_str() );
+                }
                 return -1;
             }
         }
